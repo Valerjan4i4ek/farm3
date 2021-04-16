403a264f1a3c24db61aae93f5a7562fb615fbb39
Valerjan4i4ek <75137524+Valerjan4i4ek@users.noreply.github.com>
2021-04-16 16:55:32 +0300

    Update MySQLCache.java

--- src/main/java/MySQLCache.java
+++ src/main/java/MySQLCache.java
@@ -8,13 +8,16 @@ public class MySQLCache
     private Map<Integer, Field> cache;
     MySQLClass2 sql;
     List<Integer> listCash;
+    List<Long> listTime;
 
     public MySQLCache(int cash, int fieldSize){
         this.cache = new ConcurrentHashMap<>();
         sql = new MySQLClass2();
         this.listCash = new CopyOnWriteArrayList<>();
+        this.listTime = new CopyOnWriteArrayList<>();
         init(fieldSize);
         initCash(cash);
+        initTime();
     }
 
     public void addPlant(Integer idField, Field field){
@@ -27,6 +30,24 @@ public class MySQLCache
     public void addCash(Integer cash){
         listCash.add(cash);
         sql.addCash(cash);
+    }
+
+    public void addTime(Long time){
+        listTime.add(time);
+        sql.addTime(time);
+    }
+
+    public void initTime(){
+        try{
+            List<Long> list = sql.cacheTime();
+            if(list != null && !list.isEmpty()){
+                for(long l : list){
+                    listTime.add(l);
+                }
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     public void initCash(int cash){
@@ -73,5 +94,10 @@ public class MySQLCache
 
     public Integer getCash(){
         return listCash.get(listCash.size()-1);
+    }
+
+
+    public List<Long> getTime() {
+        return listTime;
     }
 }
